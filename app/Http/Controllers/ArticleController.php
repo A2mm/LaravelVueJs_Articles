e2ac54598c810cf72598dd79c3d5933d4017461f
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-12-25 13:15:20 +0200

    added comments

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -8,11 +8,6 @@ use App\Http\Resources\Article as ArticleResource;
 
 class ArticleController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         // Get articles
@@ -22,13 +17,6 @@ class ArticleController extends Controller
         return ArticleResource::collection($articles);
     }
 
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         $article = $request->isMethod('put') ? Article::findOrFail($request->article_id) : new Article;
@@ -43,12 +31,6 @@ class ArticleController extends Controller
         
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         // Get article
@@ -58,12 +40,6 @@ class ArticleController extends Controller
         return new ArticleResource($article);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         // Get article
